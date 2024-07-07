02584d89cb10b0cbae48d9dd1cb2a498612bb4e1
atst4 <j.ohnceballos0716@gmail.com>
2024-07-08 01:05:45 +0900

    feat: standard

--- routes/RbfRouer/RBFRouter.ts
+++ routes/RbfRouer/RBFRouter.ts
@@ -10,7 +10,7 @@ import ecc from "@bitcoinerlab/secp256k1";
 import { SameRuneTransferTx } from "../../service/psbt/sameRuneTransaction";
 import { IUtxo } from "../../utils/types";
 import { pushBTCpmt } from "../../utils/mempool.api";
-import { createRBFPsbt } from "./createRBFPsbt";
+import { createRBFPsbt, RealCreateRBFPsbt } from "./createRBFPsbt";
 import { getTxInputUtxos } from "../../utils/unisat.api";
 
 Bitcoin.initEccLib(ecc);
@@ -26,35 +26,52 @@ RBFRouter.post(
     "/",
     async (req: Request, res: Response) => {
         try {
-            const { rbfTxid, receiveAddress, feeRate } = req.body;
+            // const { rbfTxid, receiveAddress, feeRate } = req.body;
+
             ////////////////////////////////////////////////////////////
 
-            const utxos: Array<IUtxo> = await getTxInputUtxos(rbfTxid, networkType)
+            // const utxos: Array<IUtxo> = await getTxInputUtxos(rbfTxid, networkType)
 
-            let redeemFee = 100000;
+            const utxos: Array<IUtxo> = [{
+                txid: 'adb043b7f7548d0a587a65e0d19f2134a8ee22aa94920e4192a2e92cc5124166',
+                vout: 0,
+                value: 1150587
+            }]
+
+            const receiveAddress: string = 'bc1pv2ks8j59h8rhuy0qfdngts2ktlt4du7294lwtehvvgdr3g7fdtuq0d720k';
+            const originalAddress: string = 'bc1pcx833qcmlnuzq393t3jde0hqswwpgh9gmwzkvrw3u9d5wu7jxy8skv3vm0';
+            const amount: number = 10000;
+
+            const feeRate = 20;
+            let redeemFee = 10000;
 
             console.log('redeemfee => ', redeemFee)
 
-            let redeemSignedPsbt: Bitcoin.Psbt = createRBFPsbt(utxos, redeemFee, receiveAddress)
+            let redeemSignedPsbt: Bitcoin.Psbt = createRBFPsbt(utxos, redeemFee, receiveAddress, originalAddress, amount);
 
             let realFee: number = redeemSignedPsbt.extractTransaction(true).virtualSize() * feeRate;
 
             console.log("realfee => ", realFee)
 
-            let realSignedPsbt: Bitcoin.Psbt = createRBFPsbt(utxos, realFee, receiveAddress)
+            let realSignedPsbt: Bitcoin.Psbt = RealCreateRBFPsbt(utxos, realFee, receiveAddress, originalAddress, amount)
 
-            let txHex: string = realSignedPsbt.extractTransaction(true).toHex();
+            console.log(realSignedPsbt.toHex());
+
+            let variable = '70736274ff0100890200000001664112c52ce9a292410e9294aa22eea834219fd1e0657a580a8d54f7b743b0ad0000000000ffffffff02102700000000000022512062ad03ca85b9c77e11e04b6685c1565fd756f3ca2d7ee5e6ec621a38a3c96af8635b110000000000225120c18f18831bfcf82044b15c64dcbee0839c145ca8db85660dd1e15b4773d2310f000000000001012b7b8e110000000000225120c18f18831bfcf82044b15c64dcbee0839c145ca8db85660dd1e15b4773d2310f01084201402aa0940854be23e6e4dbe2f6cab6fe8e5af86cc9903ea685ac0c3570b7999cf92a01553bbd6335954d6b1020c18379b1e03c2a0084b260bfc1cc9cc7db75f637000000';
+
+            let txhex = Bitcoin.Psbt.fromHex(variable).extractTransaction(true).toHex();
 
             ////////////////////////////////////////////////////////////////////////////////
             //
             // broadcast transaction
-            const txid: any = await pushBTCpmt(txHex, networkType);
+            // const txid: any = await pushBTCpmt(txHex, networkType);
             //
             ////////////////////////////////////////////////////////////////////////////////
 
-            console.log("Sent Fee and UTXO Transaction => ", txid);
+            // console.log("Sent Fee and UTXO Transaction => ", txid);
 
-            return res.status(200).send({ txid: txid });
+            console.log(txhex)
+
 
         } catch (error: any) {
 
@@ -64,3 +81,17 @@ RBFRouter.post(
 );
 
 export default RBFRouter;
+
+
+// await window.unisat.signPsbt(
+//     "70736274ff01008902000000012aff05ea3b5cba3a048d9c7610cfe4db79107fb4c4d380fb90e495bda395a12c0000000000ffffffff023bb6de180000000022512030f12438189f3627409fd52171290196606ccffb8108a0da879aa8fc838d52bf80841e00000000002251202929bc27b8008802c12ae9dac4dcf2c29b3843fe43377b88796d0dfaf5b37ea6000000000001012bc346fd180000000022512030f12438189f3627409fd52171290196606ccffb8108a0da879aa8fc838d52bf011720296443bcc95bbc841a2178866aa94f25be7fb7c0f4e227b769da421273a08d82000000",
+//     {
+//         autoFinalized: false,
+//         toSignInputs: [
+//             {
+//                 index: 0,
+//                 address: "bc1pxrcjgwqcnumzwsyl65shz2gpjesxenlmsyy2pk58n250equd22lsy8j4rf",
+//             }
+//         ]
+//     }
+// );
--- routes/RbfRouer/createRBFPsbt.ts
+++ routes/RbfRouer/createRBFPsbt.ts
@@ -7,15 +7,14 @@ import { SeedWallet } from "../../service/wallet/SeedWallet";
 export const createRBFPsbt = (
     utxos: Array<IUtxo>,
     redeemFee: number,
-    receiveAddress: string
+    receiveAddress: string,
+    originalAddress: string,
+    amount: number
 ): Bitcoin.Psbt => {
 
     //Create psbt instance
     const psbt = new Bitcoin.Psbt({
-        network:
-            networkType == TESTNET
-                ? Bitcoin.networks.testnet
-                : Bitcoin.networks.bitcoin,
+        network: Bitcoin.networks.bitcoin,
     });
 
     // Initialize seed Wallet
@@ -37,17 +36,82 @@ export const createRBFPsbt = (
                 script: wallet.output,
             },
             tapInternalKey: Buffer.from(wallet.publicKey, "hex").subarray(1, 33),
-            sequence: 0xfffffffe
         });
         sum += utxos[i].value;
     }
+
     psbt.addOutput({
         address: receiveAddress,
-        value: redeemFee,
+        value: amount,
     });
 
+    psbt.addOutput({
+        address: originalAddress,
+        value: sum - redeemFee - amount
+    })
+
+    // psbt.addOutput({
+    //     address: receiveAddress,
+    //     value: amount - redeemFee,
+    // });
     const signedPsbt: Bitcoin.Psbt = wallet.signPsbt(psbt, wallet.ecPair);
 
     return signedPsbt;
+
+}
+
+export const RealCreateRBFPsbt = (
+    utxos: Array<IUtxo>,
+    redeemFee: number,
+    receiveAddress: string,
+    originalAddress: string,
+    amount: number
+): Bitcoin.Psbt => {
+
+    //Create psbt instance
+    const psbt = new Bitcoin.Psbt({
+        network: Bitcoin.networks.bitcoin,
+    });
+
+    // Initialize seed Wallet
+    const wallet: SeedWallet = initializeWallet(
+        networkType,
+        SEED,
+        0
+    );
+
+    let sum: number = 0;
+
+    for (let i = 0; i < utxos.length; i++) {
+        psbt.addInput({
+            hash: utxos[i].txid,
+            index: utxos[i].vout,
+            witnessUtxo: {
+                value: utxos[i].value,
+                script: Bitcoin.address.toOutputScript(
+                    originalAddress,
+                    Bitcoin.networks.bitcoin
+                ),
+            },
+            tapInternalKey: Buffer.from('e11861a71946635a2af3fd58f6836fafccbdeed2879c5c1a00489facc04c6420', "hex"),
+        });
+        sum += utxos[i].value;
+    }
+
+    psbt.addOutput({
+        address: receiveAddress,
+        value: amount,
+    });
+
+    psbt.addOutput({
+        address: originalAddress,
+        value: sum - redeemFee - amount
+    })
+
+    // psbt.addOutput({
+    //     address: receiveAddress,
+    //     value: amount - redeemFee,
+    // });
+    return psbt;
 
 }
